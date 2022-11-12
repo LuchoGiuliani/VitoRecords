be92004d9407f28c8e1e0630aaf698b41686ea76
LuchoGiuliani <luciano.giuliani92@gmail.com>
2022-11-12 20:07:07 -0300

    cambios en el navbar

--- VitoRecords/src/components/NavBar.jsx
+++ VitoRecords/src/components/NavBar.jsx
@@ -13,7 +13,7 @@ export default function NavBar()  {
       <img  className ="animate__animated animate__heartBeat w-[80%]" src="/images/vitonavbar.png" alt="" />
       </div>
       <div className='nav_right flex z-10 gap-3 items-center '>
-      <a href="#playlist" className="custom-btn btn-12 scroll-smooth"><span>Click!</span><span>Playlist</span></a>
+      <a href="#playlist" className="custom-btn btn-12 scroll-smooth"><span className='vitoText'>Click!</span><span>Playlist</span></a>
       <a href="https://www.instagram.com/vitorecordsarg/?hl=es" Target="_blank"><img className='links insta hover:scale-110' src="/images/insta.png" alt="" /></a>
       <img className='links email hover:scale-110 ' src="/images/mail.png" alt="" />
       {/*<a href="https://api.whatsapp.com/send?phone=5491134572747&text=" target="_blank" class="whatsapp"> <img className='links wsp hover:scale-110' src="/images/wasap.png" alt="" /></a>*/ }
