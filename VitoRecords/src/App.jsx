360cb6f23d0e7f17c43feb795eedf02039acaa9b
LuchoGiuliani <luciano.giuliani92@gmail.com>
2022-11-15 18:00:13 -0300

    Cambios en el background y sepia image

--- VitoRecords/src/App.jsx
+++ VitoRecords/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
  
 
   return (
-    <div className="App bg-blend-color-burn">
+    <div className="App ">
      
        {/* navBar */}
        <NavBar />
--- VitoRecords/src/components/NavBar.jsx
+++ VitoRecords/src/components/NavBar.jsx
@@ -7,15 +7,15 @@ export default function NavBar()  {
 <div className="video__back">
   
  {/*<h1 className='animate__animated animate__fadeInLeftBig animate__repeat-3 text-zinc-200 vitoText text-7xl md:absolute top-[40%] left-[30%]'>VITO RECORDS</h1>*/}
-  <img className=' object-cover md:h-screen w-full ' src="/images/estudioMain.jpg" alt="" />
+  <img className=' object-cover md:h-screen w-full sepia-[.55]' src="/images/estudioMain.jpg" alt="" />
     <div className='p-4 flex navbar_container justify-between absolute top-[1%] w-[100%] '>
       <div className='img_vito z-10'>
-      <img  className ="animate__animated animate__heartBeat w-[80%]" src="/images/vitonavbar.png" alt="" />
+      <img  className ="animate__animated animate__heartBeat  " src="/images/vitonavbar.png" alt="" />
       </div>
-      <div className='nav_right flex z-10 gap-3 items-center '>
+      <div className='nav_right flex justify-center z-10 gap-3 '>
       {/* <a href="#playlist" className="scroll-smooth"><i class="fa-solid fa-folder-music"></i> </a> */}
       <a href="https://www.instagram.com/vitorecordsarg/?hl=es" Target="_blank"><img className='links insta hover:scale-110' src="/images/insta.png" alt="" /></a>
-      <img className='links email hover:scale-110 ' src="/images/mail.png" alt="" />
+      {/* <img className='links email hover:scale-110 ' src="/images/mail.png" alt="" /> */}
       {/*<a href="https://api.whatsapp.com/send?phone=5491134572747&text=" target="_blank" class="whatsapp"> <img className='links wsp hover:scale-110' src="/images/wasap.png" alt="" /></a>*/ }
       </div>
       </div>
